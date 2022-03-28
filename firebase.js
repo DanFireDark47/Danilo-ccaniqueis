4b24a2bca8c627139dfd3c756d3648857783640d
Andrei Luiz Marqueto <andrei.marqueto@etec.sp.gov.br>
2022-03-28 03:15:00 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,5 @@
 import firebase from 'firebase';
 
-// Your web app's Firebase configuration
-/*var firebaseConfig = {
-    apiKey: "AIzaSyAQyUR4T4V1Es7q7XTLve56TR7ygGEN820",
-    authDomain: "crud-firebase-react-native-exp.firebaseapp.com",
-    databaseURL: "https://crud-firebase-react-native-exp.firebaseio.com",
-    projectId: "crud-firebase-react-native-exp",
-    storageBucket: "crud-firebase-react-native-exp.appspot.com",
-    messagingSenderId: "756661446225",
-    appId: "1:756661446225:web:48f8057601d0072d7bfa70",
-    measurementId: "G-MXCGYQB4HM"
-};*/
-
 var firebaseConfig = {
 apiKey: "AIzaSyC44dsyTW6dFVED2gwsUDxohu9uj5IQK9M",
 authDomain: "reactfirebase-prof.firebaseapp.com",
